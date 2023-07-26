a2a57110dc1a75e32bfaf002b4562038e8627995
praise <essiakpevwepraise@gmail.com>
2023-07-26 01:49:12 -0700

    feat: deriving data from state declaratively

--- src/containers/SummaryContainer.js
+++ src/containers/SummaryContainer.js
@@ -3,11 +3,12 @@ import { Summary } from "../components/Summary";
 
 const mapStateToProps = state => {
     const items = state.items;
-    let subtotal  = 0;
+    //let subtotal  = 0
+    // for(const item of items) {
+    //     subtotal += item.quantity * item.price; 
+    // }
+    const subtotal = items.reduce((accumulator, item) => accumulator + item.quantity * item.price, 0);
 
-    for(const item of items) {
-        subtotal += item.quantity * item.price; 
-    }
   const tipAmount = subtotal * (state.tipPercentage / 100); 
 
   const total = subtotal + tipAmount; 
